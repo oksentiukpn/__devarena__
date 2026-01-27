ebb623840d9c79a77b368556cc47d84376d90c29
oksentiukpn <okseigri22@gmail.com>
2026-01-28 01:23:53 +0200

    added authentification and test pages

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,59 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from app import db
+from app.models import User
+from app.auth.utils import login_required
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if "user_id" in session:
+        return redirect(url_for("main.home"))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # ///
+        # Future data validation
+        # ///
+        user = User(username=username, email=email)
+        user.set_password(password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f"Account with name <{username}> was successfully created!", "success")
+        return redirect(url_for("auth.login"))
+    # if not sending data, just give html
+    return render_template("auth/register.html")
+
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login Page Placeholder"
+    if "user_id" in session:
+        return redirect(url_for("main.home"))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+            session["user_id"] = user.id
+            flash("Successfull login!", "success")
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("main.home"))
+        else:
+            flash("Login failed: Invalid password or email", "danger")
+
+    # if not sending data, just give html
+    return render_template("auth/login.html")
+
+
+@auth.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    flash("Successfull logout", "success")
+    return redirect(url_for("main.home"))
--- app/models.py
+++ app/models.py
@@ -1,19 +1,25 @@
 from datetime import datetime
-from app import db, login_manager
-from flask_login import UserMixin
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
-    password = db.Column(db.String(60), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)  # hashed of course
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
