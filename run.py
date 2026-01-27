136f78de6ecd424d3101b81e86a30a0778f58713
oksentiukpn <okseigri22@gmail.com>
2026-01-28 01:48:22 +0200

    added flexible running in debug mode

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from os import environ
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    debug_mode = environ.get("FLASK_DEBUG", "False").lower() == "true"
+    app.run(host="0.0.0.0", debug=debug_mode)
