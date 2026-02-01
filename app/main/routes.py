3c222ddb3ea78f7f4e314ea8da233c0ae6b3129c
oksentiukpn <okseigri22@gmail.com>
2026-02-01 14:52:35 +0200

    some fixes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+from flask import (Blueprint, flash, redirect, render_template, request,
+                   session, url_for)
 from sqlalchemy import case, desc
+from sqlalchemy.orm import joinedload
 
 from app import db
 from app.auth.utils import login_required
+from app.main.form import PostForm
 from app.main.utils import check_data
 from app.models import Post
 
@@ -31,9 +34,9 @@ def feed_page():
     user_languages = [lang[0] for lang in user_languages]
 
     score_case = case((Post.language.in_(user_languages), 50), else_=0)
-
     feed_posts = (
-        Post.query.filter(Post.visibility == "public")
+        Post.query.options(joinedload(Post.author))
+        .filter(Post.visibility == "public")
         .filter(Post.user_id != session["user_id"])
         .order_by(desc(score_case), Post.created_at.desc())
         .limit(20)
@@ -46,33 +49,30 @@ def feed_page():
 @main.route("/post", methods=["GET", "POST"])
 @login_required
 def post():
+    form = PostForm()
     if request.method == "POST":
-        checked = check_data(request)
-        if not isinstance(checked, bool):
-            flash(checked, "danger")
-        else:
-            # adding to db
-            feedback_list = request.form.getlist("feedback[]")
-            feedback_str = ",".join(feedback_list)
-            tags = [tag.strip() for tag in request.form.get("tags").split("#")]
-            new_post = Post(
-                title=request.form.get("project_name"),
-                description=request.form.get("description"),
-                language=request.form.get("language"),
-                code=request.form.get("code"),
-                tags=",".join(tags),
-                feedback_type=feedback_str,
-                visibility=request.form.get("visibility"),
-                user_id=session["user_id"],
-            )
+        raw_tags = form.tags.data.replace("#", " ").split()
+        clean_tags = ",".join([tag.strip() for tag in raw_tags])
+        feedback_str = ",".join(form.feedback.data)
 
-            try:
-                db.session.add(new_post)
-                db.session.commit()
-                flash("Your project has been posted!", "success")
-                return redirect(url_for("main.feed_page"))
-            except Exception as e:
-                db.session.rollback()
-                flash(f"An error occurred saving the post: {e}", "danger")
+        # 3. Create Post
+        new_post = Post(
+            title=form.project_name.data,
+            description=form.description.data,
+            language=form.language.data,
+            code=form.code.data,
+            tags=clean_tags,
+            feedback_type=feedback_str,
+            visibility=form.visibility.data,
+            user_id=session["user_id"],
+        )
 
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            flash("Your project has been posted!", "success")
+            return redirect(url_for("main.feed_page"))
+        except Exception as e:
+            db.session.rollback()
+            flash(f"An error occurred saving the post: {e}", "danger")
     return render_template("post.html")
