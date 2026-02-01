72595343a74b74d084a5c1d2a6d0ff71bc87e5c5
oksentiukpn <okseigri22@gmail.com>
2026-02-01 14:08:41 +0200

    added posting and a bit feed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
@@ -12,10 +14,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(
-        db.String(255), nullable=False, default="default.jpg")
-    password_hash = db.Column(
-        db.String(256), nullable=True)  # hashed of course
+    image_file = db.Column(db.String(255), nullable=False, default="default.jpg")
+    password_hash = db.Column(db.String(256), nullable=True)  # hashed of course
+    languages = db.Column(db.String(1024), nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -27,3 +28,23 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    language = db.Column(db.String(50), nullable=False)
+    code = db.Column(db.Text, nullable=False)
+    tags = db.Column(db.String(200))
+    feedback_type = db.Column(db.String(200))  # Stored as comma-separated string
+    visibility = db.Column(db.String(20), default="public", nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # Foreign Key linking to User
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    author = db.relationship("User", backref="posts", lazy=True)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.created_at}')"
