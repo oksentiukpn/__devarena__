738502084046e3d5a4138b37569b77f31a3a7ee7
Oleksandr <oksentiuk.pn@ucu.edu.ua>
2026-02-03 00:57:24 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -88,7 +88,7 @@ class Post(db.Model):
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, Nullable=False)
+    content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, Nullable=False, default=datetime.utcnow)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
