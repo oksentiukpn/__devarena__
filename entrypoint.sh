a61230919ae7a562ca37872e87d07b2b27bd3a13
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-02-16 02:12:03 +0200

    feat(conf): added request limits

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 set -e
 
 echo "Applying database migrations..."
+# adding wait for the database to be ready
+until nc -z -v -w30 $DB_HOST $DB_PORT; do
+  echo "Waiting for database connection..."
+  sleep 1
+done
 flask db upgrade
 
 echo "Starting Flask..."
