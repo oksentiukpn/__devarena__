9d3ac167ab611e2cba9da9206cc956be1cb33290
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-02-23 01:51:16 +0200

    feat: added CSRF

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,13 @@ from config import Config
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 migrate = Migrate()
 oauth = OAuth()
+csrf = CSRFProtect()
 
 
 def create_app(config_class=Config):
@@ -31,6 +33,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     oauth.init_app(app)
+    csrf.init_app(app)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     # app.permanent_session_lifetime = timedelta(days=14)
 
