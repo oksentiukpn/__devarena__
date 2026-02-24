d6ca81ae27888b90cfbca721c967fb0d8e68566c
Sofia Pushkarova <pushkarova.pn@ucu.edu.ua>
2026-02-24 23:21:57 +0200

    changed one moment

--- app/models.py
+++ app/models.py
@@ -51,9 +51,7 @@ class User(db.Model):
     image_file = db.Column(db.String(255), nullable=False, default="default.jpg")
     password_hash = db.Column(db.String(256), nullable=True)  # hashed of course
     languages = db.Column(db.String(1024), nullable=True)
-
-    # subscribed_to_daily_prompt = db.Column(db.Boolean, default=True, nullable=False)
-
+    subscribed_to_daily_prompt = db.Column(db.Boolean, default=True, nullable=False)
     points = db.Column(db.Integer, default=0, server_default="0", nullable=False)
 
     def set_password(self, password: str) -> None:
